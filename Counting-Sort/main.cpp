300f15409e36816e4d0a982c2c63101aec746917
Tomás Abril <tomasabril@users.noreply.github.com>
2015-04-23 22:14:36 -0300

    implement read date

---
+++ Counting-Sort/main.cpp
@@ -0,0 +1,37 @@
+/// Tomas Abril
+/// Helena Arins
+/// Andre Badenas
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+using namespace std;
+
+//variaveis globais
+int datas[1000000];
+
+//funcoes
+void lerDatas();
+void countingSort();
+void imprimir();
+
+int main()
+{
+    lerDatas();
+
+
+    return 0;
+}
+
+void lerDatas(){
+    int i;
+    for(i=0; i<1000000; i++){
+        scanf("%d", &datas[i]);
+    }
+
+}
+
+
