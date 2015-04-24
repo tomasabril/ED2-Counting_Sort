2d6e61c8b9688380da463aff71dad7cd9d14c764
Tomás Abril <tomasabril@users.noreply.github.com>
2015-04-24 12:13:14 -0300

    arrumar tamanhos
    tenho q sair e nao sei se esta funcionando

--- Counting-Sort/main.cpp
+++ Counting-Sort/main.cpp
@@ -11,11 +11,11 @@
 using namespace std;
 
 //#define TAM 1000000 // Quantas datas ler
-#define TAM 3
+#define TAM 10
 
 //variaveis globais
-int a[TAM];         // vetor a ser ordenado
-int b[TAM];         // vetor ordenado
+int a[TAM+1];         // vetor a ser ordenado
+int b[TAM+1];         // vetor ordenado
 int *c;                 // vetor auxiliar de tamanho k que quarda o numero de ocorrencias de elementos i em a
 int n = TAM;        // quantidade de numeros a serem ordenados
 int k=0;                // maior numero da lista
@@ -37,19 +37,20 @@ int main()
 void lerDatas()
 {
     int i;
-    for(i=0; i<TAM; i++) {
+    for(i=1; i<TAM+1; i++) {
         scanf("%d", &a[i]);
         if(a[i] > k) {
             k = a[i];
         }
     }
-    c = (int *)malloc(k * sizeof(int));
+    c = (int *)malloc(1 + k * sizeof(int));
 }
 
 void imprimir()
 {
+    printf("\n");
     int i;
-    for(i=0; i<TAM; i++) {
+    for(i=1; i<TAM+1; i++) {
         printf("%d\n", b[i]);
     }
 }
@@ -60,7 +61,7 @@ void countingSort()
     for(i=1; i <= k; i++) {
         c[i] = 0;
     }
-    for(j=1; j <= n; j++) {
+    for(j=1; j < n+1; j++) {
         c[a[j]] = c[a[j]] + 1;
     }
     for(i=2; i<=k; i++) {
